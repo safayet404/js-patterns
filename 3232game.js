8e84126f67c86070dcc40724e413cc5123f33520
safayet404 <hossainsafayet187@gmail.com>
2026-05-16 18:30:27 +0600

    LeetCode 3232 alice win optimized solution

--- 3232game.js
+++ 3232game.js
@@ -7,26 +7,31 @@ var canAliceWin = function (nums) {
     let single = 0
     let double = 0
 
+    // for (let num of nums) {
+    //     if (num < 10) {
+    //         single += num
+    //         console.log(num);
+
+    //     } else {
+    //         double += num
+    //     }
+    // }
+
+
+    // if (single > double) {
+    //     return true
+    // } else if (double > single) {
+    //     return true
+    // } else {
+    //     return false
+    // }
+
     for (let num of nums) {
-        if (num < 10) {
-            single += num
-            console.log(num);
-
-        } else {
-            double += num
-        }
+        if (num < 10) single += num
+        else double += num
     }
 
-
-    if (single > double) {
-        return true
-    } else if (double > single) {
-        return true
-    } else {
-        return false
-    }
-
-
+    return single !== double
 
 
 };
