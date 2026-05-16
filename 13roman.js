22b0c2d25ede2a0ef905432ff1472c55ef297ccd
safayet404 <hossainsafayet187@gmail.com>
2026-05-16 23:51:25 +0600

    LeetCode 13 roman to integer

---
+++ 13roman.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+
+    let result = 0
+
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+
+
+    for (let i = 0; i < s.length; i++) {
+        const currentValue = romanMap[s[i]]
+        const nextValue = romanMap[s[i + 1]]
+
+        if (currentValue < nextValue) {
+            result -= currentValue
+        } else {
+            result += currentValue
+        }
+    }
+
+    return result
+
+
+
+};
+
+console.log(romanToInt("IV"));
