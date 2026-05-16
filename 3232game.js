f6fd8fd227fdf48c8556f8432d1b43836d2f188b
safayet404 <hossainsafayet187@gmail.com>
2026-05-16 18:28:21 +0600

    LeetCode 3232 alice win

---
+++ 3232game.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canAliceWin = function (nums) {
+
+    let single = 0
+    let double = 0
+
+    for (let num of nums) {
+        if (num < 10) {
+            single += num
+            console.log(num);
+
+        } else {
+            double += num
+        }
+    }
+
+
+    if (single > double) {
+        return true
+    } else if (double > single) {
+        return true
+    } else {
+        return false
+    }
+
+
+
+
+};
+
+console.log(canAliceWin([5, 5, 5, 25]));
