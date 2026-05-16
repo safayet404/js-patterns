ccf0521b99a79624bf37bec70a79c70f12d9b18e
safayet404 <hossainsafayet187@gmail.com>
2026-05-16 15:16:06 +0600

    Leet code 2769 solved

---
+++ 2769.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+var theMaximumAchievableX = function (num, t) {
+    return num + 2 * t
+};
+
+console.log(theMaximumAchievableX(4, 1));
