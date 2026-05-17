5e3906278c95cafd014c196a7b7fef41bdcc0d42
safayet404 <hossainsafayet187@gmail.com>
2026-05-18 01:22:27 +0600

    27

---
+++ 17May/28string.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+
+    // for (let i = 0; i < haystack.length - 2; i++)
+    // {
+    //     if(haystack[])
+    // }
+
+    let test = "sadbutsad"
+    console.log(test.slice(test.length));
+
+
+
+};
+
+console.log(strStr("sadbutsad", "sad"));
