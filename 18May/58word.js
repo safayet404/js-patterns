472dcff556f5c54607d4838bc04724594310a5e0
safayet404 <hossainsafayet187@gmail.com>
2026-05-18 21:48:42 +0600

    LeetCode 58 length of last word

---
+++ 18May/58word.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+    return s.trim().split(" ").pop().length
+
+};
+
+console.log(lengthOfLastWord("Hello World"));
