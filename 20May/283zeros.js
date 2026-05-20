a16ca38ecf2e3ddf769ec05151b279e8ec186080
safayet404 <hossainsafayet187@gmail.com>
2026-05-20 20:49:26 +0600

    LeetCode 283 move zeros to the end of array with ascending order

---
+++ 20May/283zeros.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+    let insertPos = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[insertPos] = nums[i]
+            insertPos++
+        }
+    }
+
+    for (let i = insertPos; i < nums.length; i++) {
+        nums[i] = 0
+    }
+
+    return nums
+};
+
+console.log(moveZeroes([0, 1, 0, 3, 12]));
