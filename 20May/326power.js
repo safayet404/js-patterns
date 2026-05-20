1334f217694bcab68156845082043e26da8ecd13
safayet404 <hossainsafayet187@gmail.com>
2026-05-21 00:24:29 +0600

    LeetCode 326 Logarithmic solution

--- 20May/326power.js
+++ 20May/326power.js
@@ -6,14 +6,19 @@ var isPowerOfThree = function (n) {
 
     if (n == 1 || n === 3) return true
 
-    for (let i = 0; i < Math.floor(n / 3); i++) {
-        if (3 ** i == n) return true
+    if (Math.floor(n % 3) !== 0 && n < 0) return false
+
+    // for (let i = 0; i < Math.floor(n / 3); i++) {
+    //     if (3 ** i == n) return true
+    // }
+
+    while (n > 1) {
+        if (n % 3 !== 0) return false
+        n = n / 3
     }
 
-    return false
-
-
+    return true
 
 };
 
-console.log(isPowerOfThree(387420488));
+console.log(isPowerOfThree(0));
