5d29f8746957e6bf5e3ff2232da429d88724221f
safayet404 <hossainsafayet187@gmail.com>
2026-05-21 15:54:28 +0600

    LeetCode 231 and 342 solved

---
+++ 21May/342pow4.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function (n) {
+    if (n <= 0) return false
+
+    while (n > 1) {
+        if (n % 4 !== 0) return false
+        n = n / 4
+    }
+
+    return true
+
+};
+
+console.log(isPowerOfFour(0));
