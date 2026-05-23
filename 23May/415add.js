66e743ab8f50c6d1ee13e407f6c3749d9d49b83e
safayet404 <hossainsafayet187@gmail.com>
2026-05-24 00:13:31 +0600

    LeetCode 415 add as string and return as string

---
+++ 23May/415add.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function (num1, num2) {
+
+    let sum = Number(num1) + Number(num2)
+    return sum.toString()
+
+};
+
+console.log(addStrings("11", "123"));
