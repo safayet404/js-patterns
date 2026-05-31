6f97abe0ea51f4299eae136d4f7717a048d281a1
safayet404 <hossainsafayet187@gmail.com>
2026-06-01 00:56:15 +0600

    LeetCode 448 not solved yet

---
+++ 31May/448disappear.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function (nums) {
+
+    const sorted = nums.sort((a, b) => a - b)
+    console.log(sorted);
+
+    const dupli = [...new Set(sorted)]
+    console.log("dupli", dupli);
+    let min = dupli[0]
+    let max = dupli[dupli.length - 1]
+
+    const missing = []
+
+    for (let i = min; i <= max; i++) {
+        if (dupli[i] !== i) {
+            missing.push(i)
+        }
+    }
+
+    console.log("disappear", missing);
+
+
+
+
+};
+
+console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1, 0]));
